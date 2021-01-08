d978802c70028611a797af3bb24c5d54f8303598
Birita Yoon Vilhelm <biritayoonvilhelm@Biritas-MacBook-Pro.local>
2021-01-08 21:17:50 +0100

    burger menu!!!

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "✕";
+    }
+
+}
+
+
+
+
 window.addEventListener("load", animationStart);
 
 function animationStart() {
